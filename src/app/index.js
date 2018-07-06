3852546de9c9b80b791e656c116e85c67f67c03e
Saptarshi Dey <dey7.kol@gmail.com>
2018-07-06 09:22:27 +0530

    css class adding and change in index for initial rendering

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM,{render} from "react-dom";
 import Header from './components/Header'
-import SearchBar from './components/SearchBar'
+import LoginForm from './components/LoginForm'
 
 class App extends React.Component{
     render(){
         return (
             <div>
-                <Header imgUrl='https://media-public.fcbarcelona.com/20157/74594198/910692/1.2/910692.jpg'/>
-                <SearchBar/>
+                <Header />
+                <LoginForm />
             </div>
         )
     }
