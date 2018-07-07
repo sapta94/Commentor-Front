60faae5ec026bcda952880f47fcb00077c745b3e
Saptarshi Dey <dey7.kol@gmail.com>
2018-07-07 22:42:03 +0530

    changing header contents based on status

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM,{render} from "react-dom";
 import '../css/main.css'
 import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 
 class Header extends React.Component{
@@ -14,6 +15,13 @@ class Header extends React.Component{
         this.callAPI();
     }
 
+    onLogout(){
+        sessionStorage.setItem('userID',null);
+        sessionStorage.setItem('userName', null);
+        sessionStorage.setItem('userLastname', null);
+        window.location.href="/register"
+    }
+
     callAPI(){
         axios.get('http://commentor.test/api/comment').then(function(response){
             console.log(response)
@@ -23,30 +31,51 @@ class Header extends React.Component{
         var image = this.props.imgUrl
         var mainText = this.props.mainText
         var subText = this.props.subText
+        var userID=sessionStorage.getItem('userID')
+        var that=this;
+        console.log(userID)
 
-        return(
-            <div className="">
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a class="navbar-brand" href="#">Commentor</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Comments</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Logout</a>
-                        </li>
-                        </ul>
-                    </div>
-                    </nav>
-            </div>
-        )
+        if(userID==null||userID==undefined||userID=='null'){
+            return(
+                <div className="">
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <a class="navbar-brand" href="#">Commentor</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                            </ul>
+                        </div>
+                        </nav>
+                </div>
+            )
+            
+        }
+        else{
+            return(
+                <div className="">
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <a class="navbar-brand" href="#">Commentor</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                            <li class="nav-item active">
+                                <Link className="nav-link" to='/comments'>Comments</Link>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" onClick={()=>that.onLogout()} href="javascript:void(0)">Logout</a>
+                            </li>
+                            </ul>
+                        </div>
+                        </nav>
+                </div>
+            )
+        }
+
+        
     }
 }
 export default Header
